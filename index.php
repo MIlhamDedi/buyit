f7c647646d412237639e60be5d29f1a3d924bc88
Ilham Dedi <muhammadilhamdedi@gmail.com>
2018-04-20 09:40:20 +0800

    finish main files

--- index.php
+++ index.php
@@ -14,34 +14,41 @@
     <title>Muhammad Ilham Dedi | U1620128B</title>
   </head>
   <body style="background-color: #00ce8f; font-family: 'Crete Round', serif;">
+    <h1 style="text-align: center; color: white; font-family: 'Pacifico', cursive; font-size: 40px; padding: 10px">BuyIt</h1>
     <?php if(isset($_POST) && (isset($_POST['submit']) && $_POST['submit'] == "Submit")) { ?>
     <?php
       $total = $_POST['apple-qty'] * 69 + $_POST['orange-qty'] * 59 + $_POST['banana-qty'] * 39;
-      
-      $orderFile = fopen("order.txt", "r") or die("Unable to open file for read!");
-      $appleQty = substr(fgets($orderFile), 24, -1);
-      $orangeQty = substr(fgets($orderFile), 25, -1);
-      $bananaQty = substr(fgets($orderFile), 25, -1);
-      fclose($orderFile);
-      
-      $orderFile = fopen("order.txt", "w") or die("Unable to open file for write!");
-      $order = "Total number of apples: " . ($_POST['apple-qty']) . "\r\nTotal number of oranges: " . ($_POST['orange-qty']) . "\r\nTotal number of bananas: " . ($_POST['banana-qty']) . "\r\n";
-      fwrite($orderFile, $order);
-      fclose($orderFile);
+
+    try {
+        $orderFile = fopen("order.txt", "r");
+        $appleQty = substr(fgets($orderFile), 24, -1);
+        $orangeQty = substr(fgets($orderFile), 25, -1);
+        $bananaQty = substr(fgets($orderFile), 25, -1);
+        fclose($orderFile);
+        $orderFile = fopen("order.txt", "w");
+        $order = "Total number of apples: " . ($appleQty+$_POST['apple-qty']) . "\r\nTotal number of oranges: " . ($orangeQty+$_POST['orange-qty']) . "\r\nTotal number of bananas: " . ($bananaQty+$_POST['banana-qty']) . "\r\n";
+        fwrite($orderFile, $order);
+        fclose($orderFile);
+    } catch (Exception $e) {
+        $orderFile = fopen("order.txt", "c+") or die("Unable to open file for write!");
+        $order = "Total number of apples: " . ($_POST['apple-qty']) . "\r\nTotal number of oranges: " . ($_POST['orange-qty']) . "\r\nTotal number of bananas: " . ($_POST['banana-qty']) . "\r\n";
+        fwrite($orderFile, $order);
+        fclose($orderFile);
+    }
+
     ?>
     <div >
-      <h1 style="text-align: center; color: white; font-family: 'Pacifico', cursive; font-size: 40px; padding: 10px">BuyIt</h1>
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
-          <h5 style="text-align: center;" class="modal-title" id="exampleModalLabel">Thank You For The Purchase, <?php echo !isset($_POST['name']) || empty($_POST['name']) || is_null($_POST['name']) ? "<em>Anonymous</em>" : $_POST['name']; ?>!</h5>
+          <h5 style="text-align: center;" class="modal-title" id="exampleModalLabel">Thank You For The Purchase, <b><?php echo !isset($_POST['name']) || empty($_POST['name']) || is_null($_POST['name']) ? "<em>Anonymous</em>" : $_POST['name']; ?></b>!</h5>
           </div>
           <div class="modal-body">
                   <ul class="list-group list-group-flush">
                       <li class="list-group-item">
                           <div class="row">
                               <div class="col-sm">
-                                  Apple
+                                  Apple (x <?php echo ($_POST['apple-qty'])?>)
                               </div>
                               <div class="col-sm" style="text-align: right;">
                               <?php echo ($_POST['apple-qty'] * 69) < 100 ? ($_POST['apple-qty'] * 69) . "&#162;" : '&#36;' . number_format(($_POST['apple-qty'] * 69)/100, 2); ?>
@@ -51,7 +58,7 @@
                       <li class="list-group-item">
                           <div class="row">
                               <div class="col-sm">
-                                  Orange
+                                  Orange (x <?php echo ($_POST['orange-qty'])?>)
                               </div>
                               <div class="col-sm" style="text-align: right;">
                               <?php echo ($_POST['orange-qty'] * 59) < 100 ? ($_POST['orange-qty'] * 59) . "&#162;" : '&#36;' . number_format(($_POST['orange-qty'] * 59)/100, 2); ?>
@@ -61,7 +68,7 @@
                       <li class="list-group-item">
                           <div class="row">
                               <div class="col-sm">
-                                  Banana
+                                  Banana (x <?php echo ($_POST['banana-qty'])?>)
                               </div>
                               <div class="col-sm" style="text-align: right;">
                               <?php echo ($_POST['banana-qty'] * 39) < 100 ? ($_POST['banana-qty'] * 39) . "&#162;" : '&#36;' . number_format(($_POST['banana-qty'] * 39)/100, 2); ?>
